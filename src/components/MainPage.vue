a1aafd4f9ee4b29b5ca2e2fee9e3c6d8d3f76923
haruki7049 <tontonkirikiri@gmail.com>
2026-01-04 23:19:16 +0900

    Update

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -6,12 +6,50 @@ const todoData = [];
 
 <template>
   <header>
-    <h1>TODOTODO</h1>
+    <div id="logo">
+      <h1>TODOTODO</h1>
+      <img src="../assets/vue.svg">
+    </div>
+
+    <div id="searchTextBox">
+      <input type="text" id="search" />
+      <button type="submit">Search</button>
+    </div>
   </header>
 </template>
 
 <style scoped>
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+div#logo {
+  display: flex;
+  align-items: center;
+  justify-content: start;
+}
+
+div#searchTextBox {
+  display: flex;
+  align-items: center;
+  justify-content: start;
+}
+
 header h1 {
+  margin: 20px;
   font-size: 2.0em;
+}
+
+header img {
+  margin: 20px;
+}
+
+header input {
+  margin: 20px;
+  font-size: 18px;
+  height: 40px;
+  width: 100%;
 }
 </style>
