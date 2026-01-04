8e0465163d12ae4fa49b47da90351938c50260af
haruki7049 <tontonkirikiri@gmail.com>
2026-01-04 22:37:37 +0900

    Update

---
+++ src/components/MainPage.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+
+const todoData = [];
+</script>
+
+<template>
+  <header>
+    <h1>TODOTODO</h1>
+  </header>
+</template>
+
+<style scoped>
+header h1 {
+  font-size: 2.0em;
+}
+</style>
