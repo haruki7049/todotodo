eda6e8b07bfdc3115b6684ba9021dad786b22630
haruki7049 <tontonkirikiri@gmail.com>
2026-01-05 13:41:08 +0900

    Update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(( command, mode, isSsrBuild, isPreview ) => {
+  let base = "/";
+
+  if (mode === "production") {
+    base = "/todotodo"
+  }
+
+  return {
+    plugins: [vue()],
+    base: base,
+  };
 });
