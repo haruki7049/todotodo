ad13ee75393ef0c57e17145faa77d3befc0aebda
haruki7049 <tontonkirikiri@gmail.com>
2026-04-14 13:08:59 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,8 @@ fn main() {
 fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS } document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
         Router::<Route> {}
     }
 }
@@ -49,7 +50,6 @@ pub fn Hero() -> Element {
 fn Home() -> Element {
     rsx! {
         Hero {}
-
     }
 }
 
